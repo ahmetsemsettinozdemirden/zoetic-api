b3203ef7c86c0f40327e146832513dc99c29fba5
A. Semsettin Ozdemirden <ahmetozdemirden@gmail.com>
2020-09-21 11:26:50 +0300

    Initial commit

---
+++ src/main/java/com/zoetic/ahmetsemsettinozdemidenassigment/AhmetSemsettinOzdemidenAssigmentApplication.java
@@ -0,0 +1,13 @@
+package com.zoetic.ahmetsemsettinozdemidenassigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhmetSemsettinOzdemidenAssigmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhmetSemsettinOzdemidenAssigmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zoetic/ahmetsemsettinozdemidenassigment/AhmetSemsettinOzdemidenAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoetic.ahmetsemsettinozdemidenassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhmetSemsettinOzdemidenAssigmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
